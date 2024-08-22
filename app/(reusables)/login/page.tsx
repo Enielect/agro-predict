e0b18ac51e2561ae5a3452a39031b9520aaf6ca3
eniola <eniolafaithful57@gmail.com>
2024-08-22 12:43:02 +0100

    Feat: create login page

---
+++ app/(reusables)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../_components/LoginForm'
+
+const LoginPage = () => {
+  return (
+    <div className='px-5'>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default LoginPage
