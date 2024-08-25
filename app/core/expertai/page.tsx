f0bf0a21d359cbf6715a679fe19f712e69ac9542
eniola <eniolafaithful57@gmail.com>
2024-08-25 12:46:57 +0100

    Fix: layout of page

--- app/core/expertai/page.tsx
+++ app/core/expertai/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ExpertAiPage = () => {
-  return <main className="min-h-screen w-full">waiting for Bukunmi 😴</main>;
+  return <main className="min-h-screen w-full mt-[40px] mx-4">waiting for Bukunmi 😴</main>;
 };
 
 export default ExpertAiPage;
