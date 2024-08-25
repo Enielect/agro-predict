3462c21e51314e06048e6df1c9ab83cfc6d72ad3
eniola <eniolafaithful57@gmail.com>
2024-08-25 11:58:07 +0100

    Remove the bottomNav

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import TopNav from "@/components/TopNav";
 import { cn } from "@/lib/utils";
+import BottomNav from "@/components/BottomNav";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -19,10 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable
-        )}
+        className={cn("min-h-screen  font-sans antialiased", inter.variable)}
       >
         <TopNav />
         <div className="pt-11">{children}</div>
