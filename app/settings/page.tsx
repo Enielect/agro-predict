1149df31388c54c0b54fafafe731b8e671452c65
eniola <eniolafaithful57@gmail.com>
2024-08-25 12:04:32 +0100

    Feat: implement settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,12 @@
+import Settings from "@/components/Settings.Page";
+import React from "react";
+
+const SettingsPage = () => {
+  return (
+    <div className="mx-6">
+      <Settings />
+    </div>
+  );
+};
+
+export default SettingsPage;
