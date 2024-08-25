e69dc378549ed52fa9dcb6b3ebda5a18d5bcce58
eniola <eniolafaithful57@gmail.com>
2024-08-25 12:43:16 +0100

    Feat: Add page content to core path

---
+++ app/core/expertai/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpertAiPage = () => {
+  return <main className="min-h-screen w-full">waiting for Bukunmi 😴</main>;
+};
+
+export default ExpertAiPage;
---
+++ app/core/predict/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PredictPage = () => {
+  return <main className="min-h-screen w-full">waiting for Bukunmi 😴</main>;
+};
+
+export default PredictPage;
