f5679febfa08c46146dcd75cbbd1def896d94dfa
eniola <eniolafaithful57@gmail.com>
2024-08-25 12:50:58 +0100

    Feat: Layout agaim

--- app/core/predict/page.tsx
+++ app/core/predict/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PredictPage = () => {
-  return <main className="min-h-screen w-full mt-[40px] mx-4">waiting for Bukunmi 😴</main>;
+  return <main className="min-h-full w-full mt-[40px] mx-4">waiting for Bukunmi 😴</main>;
 };
 
 export default PredictPage;
