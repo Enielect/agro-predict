d7d01cfd497f3f60292b619912fd4f786f1dd95d
eniola <eniolafaithful57@gmail.com>
2024-09-24 17:11:12 +0100

    feat: update top nav bar

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Hamburger } from "./svgIcons";
+import { Hamburger, ProfileIcon } from "./svgIcons";
 import Link from "next/link";
+import Notifications, { UnreadBellIcon } from "./Notifications.Page";
 
 const TopNav = () => {
   return (
-    <div className="w-full fixed bg-white flex justify-between items-center px-6 border-b  py-4 border-black">
+    <div className="sm:w-[calc(100vw-60px)] w-full fixed bg-white z-10 flex justify-between items-center px-6 border-b  py-4 border-[#444444]/20">
       <Link href="/" className="font-bold">
         Agro<span className="text-[#389738]">Predict</span>
       </Link>
       <span>
-        <Hamburger />
+        <div className="flex gap-5 items-center pr-2">
+          <UnreadBellIcon />
+          <ProfileIcon />
+        </div>
       </span>
     </div>
   );
