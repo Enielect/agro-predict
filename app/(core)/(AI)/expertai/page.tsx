4c444bc1509a9f493939ff07bd6eb4972bed6dcd
eniola <eniolafaithful57@gmail.com>
2024-10-31 23:06:42 +0100

    feat: make update to add AI chat bot(base case)

--- app/(core)/(AI)/expertai/page.tsx
+++ app/(core)/(AI)/expertai/page.tsx
@@ -1,11 +1,21 @@
 import ExpertAi from "@/components/ExpertAi.Page";
 import React from "react";
+import { nanoid } from "@/lib/utils";
+import Chat from "@/components/chat";
 
+export const runtime = "edge";
 const ExpertAiPage = () => {
+  const id = nanoid();
   return (
-    <main className="min-h-full w-full ">
-      <ExpertAi />
-    </main>
+    // <main className="min-h-full w-full ">
+    //   {/* <ExpertAi /> */}
+    // </main>
+
+    // <Chat id={id} />
+    <>
+      <p>What is making this not show</p>
+      <Chat />
+    </>
   );
 };
 
