4b87ffee31fb12e9a31f61d4fc2581ed222f22bf
eniola <eniolafaithful57@gmail.com>
2024-11-01 12:09:52 +0100

    fix supabase login issue by restoring the project on the project's supabase dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 // export { auth as middleware } from "@/auth";
 //Add optional Middleware to keep the session alive, this will update the session expiry every time its called.
 
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  return await updateSession(request);
 }
 
 export const config = {
@@ -17,6 +17,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
-}
+};
