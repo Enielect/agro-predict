94a346d5559cc7dd2c309f79cb188639f482d94a
eniola <eniolafaithful57@gmail.com>
2024-11-01 12:10:41 +0100

    feat: enable chat to be visible on mobile screens

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,35 +1,35 @@
-'use client';
+"use client";
 
-import { type CoreMessage } from 'ai';
-import { useState } from 'react';
-import { continueConversation } from '@/app/actions/chat';
-import { readStreamableValue } from 'ai/rsc';
+import { type CoreMessage } from "ai";
+import { useState } from "react";
+import { continueConversation } from "@/app/actions/chat";
+import { readStreamableValue } from "ai/rsc";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export default function Chat() {
   const [messages, setMessages] = useState<CoreMessage[]>([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((m, i) => (
         <div key={i} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.role === "user" ? "User: " : "AI: "}
           {m.content as string}
         </div>
       ))}
 
       <form
-        onSubmit={async e => {
+        onSubmit={async (e) => {
           e.preventDefault();
           const newMessages: CoreMessage[] = [
             ...messages,
-            { content: input, role: 'user' },
+            { content: input, role: "user" },
           ];
 
           setMessages(newMessages);
-          setInput('');
+          setInput("");
 
           const result = await continueConversation(newMessages);
 
@@ -37,7 +37,7 @@ export default function Chat() {
             setMessages([
               ...newMessages,
               {
-                role: 'assistant',
+                role: "assistant",
                 content: content as string,
               },
             ]);
@@ -45,12 +45,14 @@ export default function Chat() {
         }}
       >
         <input
-          className="fixed bottom-0  w-full max-w-md p-2 mb-8 border border-black rounded shadow-xl"
+          className="fixed sm:bottom-0 bottom-[56px]  w-full max-w-md p-2 mb-8 border border-black rounded shadow-xl"
           value={input}
           placeholder="Say something..."
-          onChange={e => setInput(e.target.value)}
+          onChange={(e) => setInput(e.target.value)}
         />
-        <button className='bg-black fixed bottom-0 right-0 text-white p-3'>submit</button>
+        <button className="bg-black fixed bottom-0 right-0 text-white p-3">
+          submit
+        </button>
       </form>
     </div>
   );
